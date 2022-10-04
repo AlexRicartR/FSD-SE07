b04b4ff605a34601ff2d2ac0dcb361a93eb24c20
AlexRicartR <alex.ricart@outlook.com>
2022-10-04 09:34:57 +0200

    Test

---
+++ js/main.js
@@ -0,0 +1,111 @@
+// var numero1 = 1;
+
+// console.log(numero1)
+
+// var falso = false;
+
+// console.log(falso)
+
+// var cadena ="Esto es un string";
+
+// console.log(cadena);
+
+// var nombre = "Jesus";
+// var edad = 26;
+
+// if((nombre == "Jesus") && (edad == 26)){
+//     console.log("Que bien me cae este chico")
+// } else{
+//     console.log("Ya no me cae bien")
+// }
+
+//         var nombre = "Jesus";
+//         var edad = 22;
+
+//         if((nombre == "Jesus") || (edad == 26)){
+//             console.log("Que bien me cae este chico")
+//         } else{
+//             console.log("Ya no me cae bien")
+//         }
+
+//         if(nombre != "Ambrosio"){
+//             console.log("Bieeeeeeeeen");
+//         }else{
+//             console.log("Maaaaaaaal")
+//         }
+
+// var tiempoLibre = false;
+// if(!tiempoLibre)
+
+// var edadpelicula = 18
+//     if (edadpelicula >= 18) {
+//         console.log("Puedes ver la pelicula");
+//     }else{
+//     console.log("Que te pires");
+// }
+
+// var dia = "Lunes";
+
+// switch(dia){
+//     case 'Lunes':
+//         console.log("Es lunes....")
+//     break; 
+//     case 'Martes':
+//         console.log("Es martes....")
+//     break; 
+//     case 'Miercoles':
+//         console.log("Es miercoles....")
+//     break; 
+//     case 'Jueves':
+//         console.log("Es jueves....")
+//     break; 
+//     case 'Viernes':
+//         console.log("Es viernes....")
+//     break; 
+//     case 'Sabado':
+//         console.log("Es sabado....")
+//     break; 
+//     case 'Domingo':
+//         console.log("Es domingo....")
+//     break; 
+//     default:
+//         console.log("Es otro dia")
+//         break;
+// }
+// var dia = "lunes";
+
+// switch(dia){
+//     case "lunes":
+//     case "Martes":
+//     case "Miercoles":
+//     case "Jueves":
+//     case "Viernes":
+//         console.log("Entre semana")
+//     break;
+
+//     case "Sabado":
+//     case "Domingo":
+//         console.log("Es fin de semana")
+//     break;
+
+// }
+
+var mes = "agosto";
+
+switch(dia){
+    case "junio":
+    case "julio":
+    case "agosto":
+        console.log("Es verano")
+    break;
+    case "septiembre":
+    case "octubre":
+    case "octubre":
+        console.log("Es otoño")
+    break;
+    case "marzo":
+    case "abril":
+    case "mayo":
+        console.log("Es primavera")
+    break;
+}
---
+++ js/tareas.js
@@ -0,0 +1,40 @@
+// //primer ejercicio//
+// var numero1 = 10;
+// var numero2 = 35;
+// if (numero1 > numero2) {
+//     console.log("numero 2 es mayor");
+// }
+// else if (numero2 == numero1) {
+//     console.log("son iguales");
+// }
+// else {
+//     console.log("numero1 es mayor");
+// }
+// //segundo ejercicio//
+// var nombre = "manu";
+// if (nombre == "manu") {
+//     console.log("bienvenido manu")
+// }
+// else{
+//     console.log("no eres bienvenido")
+// }
+//tercer ejercicio//
+// var nombre2 = prompt("cual es tu nombre")
+// if (nombre2 == "manu") {
+//     console.log("bienvenido manu")
+// }
+// else{
+//     console.log("no eres bienvenido")
+// }
+//cuarto ejercicio//
+// var radio = parseInt(prompt("cual es tu radio"));
+// var area = (3.14*(radio**2));
+// console.log(area);
+//quinto ejercicio//
+var numero1= parseInt(prompt("que numero es?"));
+if (numero1 % 2 == 0) {
+    console.log("par")
+}
+else{
+    console.log("es impar")
+}
